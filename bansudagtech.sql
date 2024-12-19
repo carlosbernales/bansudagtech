dceb554320f8b5398dacf8a6fc8284ccc96270e7
carlosbernales <115618742+carlosbernales@users.noreply.github.com>
2024-12-19 21:51:25 +0800

    up

--- bansudagtech.sql
+++ bansudagtech.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Dec 18, 2024 at 01:55 PM
+-- Generation Time: Dec 19, 2024 at 01:50 PM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -104,6 +104,53 @@ INSERT INTO `announcement_user` (`int`, `title`, `content`, `user_id`, `status`)
 (1, 'dfdgdg', 'fgdgdfgdfg', 1, 'unread'),
 (2, 'dfdgdg', 'fgdgdfgdfg', 2, 'unread');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `farms`
+--
+
+CREATE TABLE `farms` (
+  `id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `rsbsa` varchar(255) NOT NULL,
+  `fullname` varchar(255) NOT NULL,
+  `commodity` varchar(255) NOT NULL,
+  `farm_type` varchar(255) NOT NULL,
+  `location` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `farms`
+--
+
+INSERT INTO `farms` (`id`, `user_id`, `rsbsa`, `fullname`, `commodity`, `farm_type`, `location`) VALUES
+(1, 2, 'sd-sf-sg-fg-s2', 'wqewqe adas dasdas Jr.', 'CROP', 'RICE', '85XV+89 Calapan, Oriental Mindoro, Philippines'),
+(2, 2, 'sd-sf-sg-fg-s2', 'wqewqe adas dasdas Jr.', 'LIVESTOCK', 'RICE', '4PVV+J5 Badgerys Creek NSW, Australia');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `farms_images`
+--
+
+CREATE TABLE `farms_images` (
+  `id` int NOT NULL,
+  `farms_fk_id` int NOT NULL,
+  `image` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `farms_images`
+--
+
+INSERT INTO `farms_images` (`id`, `farms_fk_id`, `image`) VALUES
+(1, 1, 'farm_676403d911b992.35060159.jfif'),
+(2, 1, 'farm_676403d9138e76.00955169.jfif'),
+(3, 1, 'farm_676403d914a757.10631849.jfif'),
+(4, 2, 'farm_676408dfa4e3e8.37346152.jfif'),
+(5, 2, 'farm_676408dfa60e63.77819935.jfif');
+
 --
 -- Indexes for dumped tables
 --
@@ -127,6 +174,18 @@ ALTER TABLE `announcement_user`
   ADD PRIMARY KEY (`int`);
 
 --
+-- Indexes for table `farms`
+--
+ALTER TABLE `farms`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `farms_images`
+--
+ALTER TABLE `farms_images`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -147,6 +206,18 @@ ALTER TABLE `announcement`
 --
 ALTER TABLE `announcement_user`
   MODIFY `int` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `farms`
+--
+ALTER TABLE `farms`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `farms_images`
+--
+ALTER TABLE `farms_images`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
