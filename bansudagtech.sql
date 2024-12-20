161916d4bd6b176f59d9441be7dc9fa1d4afc0c2
carlosbernales <115618742+carlosbernales@users.noreply.github.com>
2024-12-20 11:43:49 +0800

    up

--- bansudagtech.sql
+++ bansudagtech.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Dec 19, 2024 at 01:50 PM
+-- Generation Time: Dec 20, 2024 at 03:43 AM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -60,7 +60,7 @@ CREATE TABLE `account` (
 --
 
 INSERT INTO `account` (`id`, `firstname`, `middlename`, `lastname`, `suffix`, `fullname`, `role`, `contact`, `email`, `password`, `birthdate`, `rsbsa`, `fourps`, `indigenous`, `tribe_name`, `pwd`, `sex`, `arb`, `region`, `province`, `municipality`, `barangay`, `org_name`, `tot_male`, `tot_female`) VALUES
-(1, 'sadasdasf', 'faasfad', 'ffdsfd', 'Jr.', 'sadasdasf faasfad ffdsfd Jr.', 'user', 3456789, NULL, NULL, '2024-12-18', '23-45-79-92-81', 'NO', 'NO', 'sdasdasdas', 'NO', 'MALE', NULL, 'sdfghjdasdas', 'sdasdasd', 'asdasda', 'dadasd', 'asdasdas', 3, 3),
+(1, NULL, NULL, NULL, NULL, NULL, 'admin', NULL, 'admin@admin.com', '$2y$12$TNnzK8GMWWhwwQWsoNqKvun.QkLLNr06EOSulxNgT1tLUhdlQEt1O', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
 (2, 'wqewqe', 'adas', 'dasdas', 'Jr.', 'wqewqe adas dasdas Jr.', 'user', 2345678, 'sarahelmenzo13@gmail.com', '$2y$12$gCP36oz8VULIlvm5Lx8I3ecufdDJXr9YTDZ6JuPBpqUj7oaPyTP3m', NULL, 'sd-sf-sg-fg-s2', 'YES', 'NO', NULL, 'YES', 'MALE', 'YES', 'asfsdfsdfsd', 'asdasd', 'asdasdasd', 'sdasd', 'asdasdas', 3, 2);
 
 -- --------------------------------------------------------
@@ -125,8 +125,8 @@ CREATE TABLE `farms` (
 --
 
 INSERT INTO `farms` (`id`, `user_id`, `rsbsa`, `fullname`, `commodity`, `farm_type`, `location`) VALUES
-(1, 2, 'sd-sf-sg-fg-s2', 'wqewqe adas dasdas Jr.', 'CROP', 'RICE', '85XV+89 Calapan, Oriental Mindoro, Philippines'),
-(2, 2, 'sd-sf-sg-fg-s2', 'wqewqe adas dasdas Jr.', 'LIVESTOCK', 'RICE', '4PVV+J5 Badgerys Creek NSW, Australia');
+(4, 2, 'sd-sf-sg-fg-s2', 'wqewqe adas dasdas Jr.', 'LIVESTOCK', 'SHEEP', 'VG43+76 Lakesland NSW, Australia'),
+(5, 2, 'sd-sf-sg-fg-s2', 'wqewqe adas dasdas Jr.', 'CROP', 'CORN', '203bde brg pag asa, Bansud, Oriental Mindoro, Philippines');
 
 -- --------------------------------------------------------
 
@@ -145,11 +145,11 @@ CREATE TABLE `farms_images` (
 --
 
 INSERT INTO `farms_images` (`id`, `farms_fk_id`, `image`) VALUES
-(1, 1, 'farm_676403d911b992.35060159.jfif'),
-(2, 1, 'farm_676403d9138e76.00955169.jfif'),
-(3, 1, 'farm_676403d914a757.10631849.jfif'),
-(4, 2, 'farm_676408dfa4e3e8.37346152.jfif'),
-(5, 2, 'farm_676408dfa60e63.77819935.jfif');
+(7, 4, 'farm_6764c7a3b26520.40637925.jpg'),
+(8, 4, 'farm_6764c7a3b3a155.30621911.jpg'),
+(9, 5, 'farm_6764e7251fe817.72840370.jpg'),
+(10, 5, 'farm_6764e72522e9f9.66566891.jpg'),
+(11, 5, 'farm_6764e72525a485.39099922.jpg');
 
 --
 -- Indexes for dumped tables
@@ -211,13 +211,13 @@ ALTER TABLE `announcement_user`
 -- AUTO_INCREMENT for table `farms`
 --
 ALTER TABLE `farms`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `farms_images`
 --
 ALTER TABLE `farms_images`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
